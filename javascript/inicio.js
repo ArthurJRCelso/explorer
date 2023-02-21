f8bb8637bd0b1a7c2a44cd6211daac5c5c20440d
Arthur Rodrigues <121518722+ArthurJRCelso@users.noreply.github.com>
2023-02-21 19:16:27 +0000

    att de pastas

---
+++ javascript/inicio.js
@@ -0,0 +1,21 @@
+console.log('Bem vindos ao Starter!!')
+
+console.log("Arthur, meu parceiro")
+
+console.log("-33")
+
+console.log(12.5 + 12)
+
+console.log(12.5 / "asdf")
+
+console.log(12.5 === "asdf")
+
+console.log("asdf" === "asdf")
+
+console.log(Infinity)
+
+console.log(undefined)
+
+console.log(null)
+
+console.log(null === undefined)
