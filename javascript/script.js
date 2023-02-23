2c63f870b76f1029cae78426a87ee48a0d345851
Arthur Rodrigues <121518722+ArthurJRCelso@users.noreply.github.com>
2023-02-23 21:31:40 +0000

    anotações

--- javascript/script.js
+++ javascript/script.js
@@ -1,7 +1,35 @@
-console.log({
+let weigth
+
+console.log(typeof weigth)
+
+let name = "Arthur"
+let age = 26
+let stars = 4.8
+let isSubscribed = true
+
+let student = {
   name: "Arthur",
-  idade: 36,
-  andar: function() {
-    console.log('andar')
-  }
-})
+  age: 26,
+  weigth: 90.4,
+  isSubscribed: true,
+}
+
+console.log(`${student.name} de idade ${student.age} pesa ${student.weigth} kg `)
+
+let students = []
+
+students = [
+  students
+]
+
+console.log(students)
+
+console.log(students[0])
+
+const john = {
+  name: "John",
+  age: 23,
+  weigth: 74.8,
+  isSubscribed: true,
+}
+
