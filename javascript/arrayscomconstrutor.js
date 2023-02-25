b771dda7347ceb38b0f94f5c3fa426bf6a79b15b
Arthur Rodrigues <121518722+ArthurJRCelso@users.noreply.github.com>
2023-02-25 13:57:30 +0000

    manipulação de dados de array

---
+++ javascript/arrayscomconstrutor.js
@@ -0,0 +1,5 @@
+// Manipulando Arrays
+
+//Criar array com construtor
+let myArray = new Array('a', 'b', 'c')
+console.log(myArray)
---
+++ javascript/detectandopalavrasemfrases.js
@@ -0,0 +1,5 @@
+// Manipulando strings
+
+// Verificar se o texto contém a palavra Amor
+let phrases = "Eu quero viver o amor!"
+console.log(phrases.includes("amor"))
