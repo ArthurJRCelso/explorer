9f5a8a3e52928f0475004ca1cd47c735def006a5
Arthur Rodrigues <121518722+ArthurJRCelso@users.noreply.github.com>
2023-02-25 17:28:47 +0000

    operadores

---
+++ javascript/expressões-operadores/operadoresstrings.js
@@ -0,0 +1,10 @@
+// Operador de String (String operator)
+
+// comparison (comparação)
+// console.log( 'a' == 'a')
+
+// concatenation (concatenação)
+// Retorna a união de Duas Strings
+let alpha = 'alpha'
+alpha += 'bet'
+console.log(alpha)
