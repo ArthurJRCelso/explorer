a0f2dc850cead833ab1c2d950132e2203d1142bc
Arthur Rodrigues <121518722+ArthurJRCelso@users.noreply.github.com>
2023-02-28 20:28:29 +0000

    script

---
+++ javascript/logicaealgoritmo/01/script.js
@@ -0,0 +1,8 @@
+/* 
+  Solicite 2 números, faça a soma deles
+  e apresente o resultado final ao usuário
+*/
+
+alert("Iremos somar 2 números")
+let numberOne = prompt("Digite o primeiro número:")
+let numberTwo = prompt("Digite o segundo número:")
