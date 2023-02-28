8d242f2e489f2a6f2e6397d0f847aace127e370c
Arthur Rodrigues <121518722+ArthurJRCelso@users.noreply.github.com>
2023-02-28 20:11:49 +0000

    function

--- javascript/estruturasderepeticao/script.js
+++ javascript/estruturasderepeticao/script.js
@@ -1,13 +1,12 @@
-// for...of
+// for...in
 
-let name = 'Arthur'
-let names = ['Arthur', 'Camila', 'Heitor']
-
-for(let char of name) {
-  console.log(char)
+let person = {
+  name: 'Arthur',
+  age: 26,
+  weight: 87.4
 }
 
-// Array
-for(let name of names) {
-  console.log(name)
+for(let property in person) {
+  console.log(property)
+  console.log(person[property])
 }
