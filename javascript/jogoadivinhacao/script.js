ea35756da891a16ae8a2e14e996b2abdc363d228
Arthur Rodrigues <121518722+ArthurJRCelso@users.noreply.github.com>
2023-03-10 23:26:44 +0000

    callback

--- javascript/jogoadivinhacao/script.js
+++ javascript/jogoadivinhacao/script.js
@@ -2,21 +2,26 @@ const screen1 = document.querySelector(".screen1")
 const screen2 = document.querySelector(".screen2")
 const btnTry = document.querySelector('#btnTry')
 const btnReset = document.querySelector('#btnReset')
-const randomNumber = Math.round(Math.random() * 10)
+let randomNumber = Math.round(Math.random() * 10)
 let xAttempts = 1
+let input1 = document.querySelector('#input1')
 
 // Eventos
 
 btnTry.addEventListener('click', handleTryClick)
 btnReset.addEventListener('click', handleResetClick)
+document.addEventListener('keydown', handleKeyDown)
 
 // Função callback
-
 function handleTryClick(event) {
   
   event.preventDefault()
 
   const inputNumber = document.querySelector("#inputNumber")
+
+  if(inputNumber.value < 0 || inputNumber.value > 10 || inputNumber.value == '') {
+    alert('Número inválido!')
+  }
 
   if(Number(inputNumber.value) == randomNumber) {
     toggleScreen()
@@ -25,10 +30,19 @@ function handleTryClick(event) {
 
   inputNumber.value = ''
   xAttempts++
+  
+  
+}
+
+function handleKeyDown(e) {
+  if (e.key == 'Enter' && screen1.classList.contains('hide')) {
+    handleResetClick()
+  }
 }
 
 function handleResetClick() {
   toggleScreen()
+  randomNumber = Math.round(Math.random() * 10)
   xAttempts = 1
 }
 
@@ -36,4 +50,5 @@ function toggleScreen() {
   screen1.classList.toggle('hide')
   screen2.classList.toggle('hide')
 }
+
 
