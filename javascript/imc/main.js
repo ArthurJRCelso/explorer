1ea2fecd1237268362b54c6ce97ffb252a3183bf
Arthur Rodrigues <121518722+ArthurJRCelso@users.noreply.github.com>
2023-03-13 22:57:06 +0000

    modalBtnClose.onclick = () =>

--- javascript/imc/main.js
+++ javascript/imc/main.js
@@ -1,8 +1,11 @@
 // variáveis - variables //
-
 const form = document.querySelector('form')
 const inputWeight = document.querySelector('#weight')
 const inputHeight = document.querySelector('#height')
+
+const modalWrapper = document.querySelector('.modal-wrapper')
+const modalMessage = document.querySelector('.modal .tittle span')
+const modalBtnClose = document.querySelector('.modal button.close')
 
 // 3 maneiras de atribuir função a um evento
 form.onsubmit = function(event) {
@@ -10,6 +13,19 @@ form.onsubmit = function(event) {
 
   const weight = inputWeight.value
   const height = inputHeight.value
-  
+
+  const result = IMC(weight, height)
+  const message = `Seu IMC é de ${result}`
+
+  modalMessage.innerHTML = message
+  modalWrapper.classList.add('open')
+}
+
+modalBtnClose.onclick = () => {
+  modalWrapper.classList.remove('open')
+}
+
+function IMC(weight, height) {
+  return (weight / ((height / 100) ** 2)).toFixed(2)
 }
 
