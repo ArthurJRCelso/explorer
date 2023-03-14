a2e50dfc38705fe3d53379c38d032fd6bf5c02bf
Arthur Rodrigues <121518722+ArthurJRCelso@users.noreply.github.com>
2023-03-14 19:51:16 +0000

    classList.add/remove

--- javascript/imc/main.js
+++ javascript/imc/main.js
@@ -3,9 +3,19 @@ const form = document.querySelector('form')
 const inputWeight = document.querySelector('#weight')
 const inputHeight = document.querySelector('#height')
 
-const modalWrapper = document.querySelector('.modal-wrapper')
-const modalMessage = document.querySelector('.modal .tittle span')
-const modalBtnClose = document.querySelector('.modal button.close')
+const modal = {
+
+  wrapper: document.querySelector('.modal-wrapper'),
+  message: document.querySelector('.modal .tittle span'),
+  buttonClose: document.querySelector('.modal button.close'),
+
+  open() {
+    modal.wrapper.classList.add('open')
+  },
+  close() {
+    modal.wrapper.classList.remove('open')
+  }
+}
 
 // 3 maneiras de atribuir função a um evento
 form.onsubmit = function(event) {
@@ -17,12 +27,12 @@ form.onsubmit = function(event) {
   const result = IMC(weight, height)
   const message = `Seu IMC é de ${result}`
 
-  modalMessage.innerHTML = message
-  modalWrapper.classList.add('open')
+  modal.message.innerHTML = message
+  modal.open()
 }
 
-modalBtnClose.onclick = () => {
-  modalWrapper.classList.remove('open')
+modal.buttonClose.onclick = () => {
+  modal.close()
 }
 
 function IMC(weight, height) {
